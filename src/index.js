85ef01acbcbaf67448c9e5a468505a899a539c2c
morita mai <mai.20.04.ab@gmail.com>
2020-10-12 17:41:51 +0900

    ゲーム自体が成立（細かい部分の調整必要）

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ function Square(props) {
       {props.value}
     </button>
   );
-  //良くわからない：className={`square ${props.isHighlight ? "highlight" : ""}`}//
 }
 
-const FnBoard = (props) => {
+const Board = (props) => {
   const renderSquare = (i, isHighlight = false) => {
     return (
       <Square
@@ -46,7 +45,7 @@ const FnBoard = (props) => {
   );
 };
 
-const FnGame = () => {
+const Game = () => {
   const [history, setHistory] = useState([
     {
       squares: Array(9).fill(null),
@@ -57,32 +56,22 @@ const FnGame = () => {
   const [isAsc, setIsAsc] = useState(true);
 
   const handleClick = (i) => {
-    const history = history.slice(0, stepNumber + 1);
+    const historyClick = history.slice(0, stepNumber + 1);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares,
-          col: (i % 3) + 1,
-          row: Math.floor(i / 3) + 1,
-        },
-      ]),
-      stepNumber: history.length,
-      xIsNext: !xIsNext,
-    });
+
+    setHistory(history.concat([{ squares }]));
+    setStepNumber(history.length);
+    setXIsNext(!xIsNext);
   };
+
   const jumpTo = (step) => {
     stepNumber(step);
     xIsNext(step % 2 === 0);
-    // setState({
-    //   stepNumber: step,
-    //   xIsNext: step % 2 === 0,
-    // });
   };
   const toggleAsc = () => {
     setIsAsc(!isAsc);
@@ -121,7 +110,7 @@ const FnGame = () => {
   return (
     <div className="game">
       <div className="game-board">
-        <FnBoard
+        <Board
           squares={current.squares}
           onClick={(i) => handleClick(i)}
           highlightCells={settlement ? settlement.line : []}
@@ -137,109 +126,8 @@ const FnGame = () => {
     </div>
   );
 };
-
-class Game extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-        },
-      ],
-      xIsNext: true,
-      stepNumber: 0,
-      isAsc: true,
-    };
-  }
-  handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares,
-          col: (i % 3) + 1,
-          row: Math.floor(i / 3) + 1,
-        },
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
-    });
-  }
-  //良くわからない：===が？のまま//
-  toggleAsc() {
-    this.setState({
-      asc: !this.state.asc,
-    });
-  }
-  //良くわからない：!の意味//
-  render() {
-    const history = this.state.history;
-    const current = history[this.state.stepNumber];
-    const settlement = calculateWinner(current.squares);
-
-    let status;
-    if (settlement) {
-      if (settlement.isDraw) {
-        status = "Draw";
-      } else {
-        status = "Winner: " + settlement.winner;
-      }
-    } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
-    }
-
-    const moves = history.map((step, move) => {
-      const desc = move
-        ? "Move #" + move + "(" + step.col + "," + step.row + ")"
-        : "Game start";
-      return (
-        <li key={move}>
-          <button
-            onClick={() => this.jumpTo(move)}
-            className={this.state.stepNumber === move ? "bold" : ""}
-          >
-            {desc}
-          </button>
-        </li>
-      );
-    });
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <FnBoard
-            squares={current.squares}
-            onClick={(i) => this.handleClick(i)}
-            highlightCells={settlement ? settlement.line : []}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <div>
-            <button onClick={() => this.toggleAsc()}>ASC⇔DESC</button>
-          </div>
-          <ol>{this.state.asc ? moves : moves.reverse()}</ol>
-        </div>
-      </div>
-    );
-    //良くわからない：()[]の違い的な部分//
-  }
-}
 // ========================================
-ReactDOM.render(<FnGame />, document.getElementById("root"));
+ReactDOM.render(<Game />, document.getElementById("root"));
 
 function calculateWinner(squares) {
   const lines = [
@@ -261,7 +149,6 @@ function calculateWinner(squares) {
         line: [a, b, c],
       };
     }
-    //良くわからない：ifの中身の部分//
   }
 
   if (squares.filter((e) => !e).length === 0) {
